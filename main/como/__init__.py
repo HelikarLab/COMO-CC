1b7f045c479bb195dbc3a3a621904d43469c26fe
Jozef Karabelly <jkarabelly2@unl.edu>
2024-03-05 15:04:58 +0100

    add R packages installation

--- main/como/__init__.py
+++ main/como/__init__.py
@@ -1,1 +1,13 @@
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector
+
 from .como_utilities import stringlist_to_list
+
+utils = rpackages.importr('utils')
+utils.chooseCRANmirror(ind=1)
+
+# Install R packages
+packnames = ('BiocManager', 'DESeq2', 'data.table', 'dplyr', 'ggplot2', 'pheatmap', 'readr', 'tibble', 'tidyr', 'tidyverse')
+names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
+if len(names_to_install) > 0:
+    utils.install_packages(StrVector(names_to_install))
