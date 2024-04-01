f9a29175a8b3666beb6b22a7767fb2ffeac5259d
Jozef Karabelly <jkarabelly2@unl.edu>
2024-04-01 15:46:10 +0200

    create singleton config

--- main/como/project.py
+++ main/como/project.py
@@ -1,6 +1,37 @@
 import os
 from pathlib import Path
 
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Config(metaclass=SingletonMeta):
+    def __init__(self, data_dir: Path = None, config_dir: Path = None, result_dir: Path = None) -> None:
+        self.data_dir = data_dir
+        if self.data_dir is None:
+            self.data_dir = Path.cwd() / "data"
+            self.data_dir.mkdir(exist_ok=True)
+
+        self.config_dir = config_dir
+        if self.config_dir is None:
+            self.config_dir = Path.cwd() / "data" / "config_sheets"
+            self.config_dir.mkdir(exist_ok=True)
+
+        self.result_dir = result_dir
+        if self.result_dir is None:
+            self.result_dir = Path.cwd() / "data" / "results"
+            self.result_dir.mkdir(exist_ok=True)
+
 
 class Configs:
     project_dir: str = None
