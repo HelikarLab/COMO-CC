87cfa1aaddc499f261dd4b92412a14fc20301e6b
Josh Loecker <joshloecker@icloud.com>
2024-10-09 12:31:40 -0500

    Add a `code_dir` for accessing R scripts, remove `Configs`

--- main/como/project.py
+++ main/como/project.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+
 class SingletonMeta(type):
     _instances = {}
 
@@ -16,7 +17,7 @@ class SingletonMeta(type):
 
 
 class Config(metaclass=SingletonMeta):
-    def __init__(self, data_dir: Path = None, config_dir: Path = None, result_dir: Path = None) -> None:
+    def __init__(self, data_dir: Path = None, config_dir: Path = None, result_dir: Path = None, code_dir: Path = None) -> None:
         self.data_dir = data_dir
         if self.data_dir is None:
             self.data_dir = Path.cwd() / "data"
@@ -32,37 +33,6 @@ class Config(metaclass=SingletonMeta):
             self.result_dir = Path.cwd() / "data" / "results"
             self.result_dir.mkdir(exist_ok=True)
 
-
-class Configs:
-    project_dir: str = None
-    
-    def __init__(self) -> None:
-        """
-        This class will hold all of the project configurations.
-        """
-        if Configs.project_dir is None:
-            Configs.project_dir = self._find_project_dir()
-        self.root_dir: str = str(Configs.project_dir)
-        
-        self.data_dir: str = os.path.join(self.root_dir, "data")
-        self.config_dir: str = os.path.join(self.data_dir, "config_sheets")
-        self.results_dir: str = os.path.join(self.data_dir, "results")
-        self.src_dir: str = os.path.join(self.root_dir, "como")
-    
-    def _find_project_dir(self) -> Path:
-        # Determine if "main" is in the current directory (i.e., `ls .`)
-        if "main" in os.listdir(os.getcwd()):
-            return Path(os.getcwd(), "main")
-        
-        # If "main" is not in the current directory, see if we are in a sub-directory of "main" (i.e., `ls ..`)
-        current_directory = Path.cwd()
-        directory_parts = current_directory.parts
-        for i, directory in enumerate(directory_parts):
-            if directory == "main":
-                return Path(*directory_parts[0:i + 1])
-        
-        # If we are not in a sub-directory of "main", then we are not in the project directory
-        raise FileNotFoundError(
-            "Could not find project directory. "
-            "Please ensure you are in the project directory, such as 'COMO' or 'main'."
-        )
+        self.code_dir = code_dir
+        if self.code_dir is None:
+            self.code_dir = Path.cwd() / "como"
