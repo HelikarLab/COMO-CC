2aa4b6f12634952a992398b9c190a35cd18f5564
Josh Loecker <joshloecker@icloud.com>
2024-10-09 13:08:21 -0500

    Remove unnecessary function, whitepace formatting using ruff

--- main/como/combine_distributions.py
+++ main/como/combine_distributions.py
@@ -2,29 +2,18 @@ from pathlib import Path
 from typing import Literal, Union
 
 
-def get_batch_name(filename: Union[str, Path]):
-    basename = os.path.basename(filename)
-    # Convert R to python
-    # return(substring(basename(x), 1, nchar(basename(x))-4))
-    return basename[:-4]
-
-
 def parse_contexts_zfpkm(wd: Union[str, Path], contexts: list[str], prep: Literal["mrna", "total"]):
     wd: Path = Path(wd)
-    
+
     batches = []
     for context in contexts:
         dir_name = Path(wd, context, prep)
         files = dir_name.glob(f"zFPKM_Matrix_{prep}_*.csv")
-        batches += [get_batch_name(file) for file in files]
-    
+        batches += [Path(file).stem for file in files]
+
     return batches
 
 
-if __name__ == '__main__':
-    result = parse_contexts_zfpkm(
-        wd="/Users/joshl/PycharmProjects/COMO/main/data/results",
-        contexts=["naiveB"],
-        prep="total"
-    )
+if __name__ == "__main__":
+    result = parse_contexts_zfpkm(wd="/Users/joshl/PycharmProjects/COMO/main/data/results", contexts=["naiveB"], prep="total")
     print(result)
