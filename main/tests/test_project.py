2314bac867017e078e7098ea42d4d8238f6f39df
Josh Loecker <joshloecker@icloud.com>
2024-10-09 15:03:12 -0500

    Skip parsing rnaseq args tests because argv has been removed

--- main/tests/test_project.py
+++ main/tests/test_project.py
@@ -1,5 +1,5 @@
 import os
-import sys
+from pathlib import Path
 
 # Add parent directory to path, allows us to import the "project.py" file from the parent directory
 # From: https://stackoverflow.com/a/30536516/13885200
@@ -8,11 +8,10 @@ from como import project
 
 
 def test_config():
-    configs = project.Configs()
-    work_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    
-    assert configs.root_dir == work_dir
-    assert configs.data_dir == os.path.join(work_dir, "data")
-    assert configs.config_dir == os.path.join(work_dir, "data", "config_sheets")
-    assert configs.results_dir == os.path.join(work_dir, "data", "results")
-    assert configs.src_dir == os.path.join(work_dir, "src")
+    configs = project.Config()
+    current_dir = Path(__file__).parent
+
+    assert configs.data_dir == current_dir / "data"
+    assert configs.config_dir == current_dir / "data" / "config_sheets"
+    assert configs.result_dir == current_dir / "data" / "results"
+    assert configs.code_dir == current_dir / "como"
